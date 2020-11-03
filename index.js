1e535b22512580195d71f505a1b63ac95586d724
richard savala <59234279+thrillhobaby@users.noreply.github.com>
2020-11-03 09:28:28 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const button = document.getElementById('github-button');
+
+button.onmouseover = function() {
+    this.style.backgroundColor = '#f6f8fa';
+}
+
+button.onmouseout = function() {
+    this.style.backgroundColor = '#24292e';
+}
